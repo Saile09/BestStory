96d2c00a3d97125a7b67ab88b5c499d7b93975bb
Armando <armandosaile09@gmail.com>
2024-06-12 21:29:04 -0600

    Add project files.

---
+++ BestStory/Controllers/StoryController.cs
@@ -0,0 +1,23 @@
+﻿using BestStory.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoryController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public StoryController(ILogger<WeatherForecastController> logger)
+        {
+            this._logger = logger;
+        }
+        [HttpGet]
+        [ProducesResponseType<List<Story>>(StatusCodes.Status200OK)]
+        public IActionResult GetStories()
+        {
+            return Ok(new List<Story>());
+        }
+    }
+}
